e68d0e547a3a750e2c0bd1d56a9d45c1f37aedee
Sam Hilton-Jones <sam.hiltonjones@gmail.com>
2024-09-01 02:47:14 +0100

    first commit

---
+++ scripts/continuous_path_scripts/sac.py
@@ -0,0 +1,127 @@
+import os
+import numpy as np
+from datetime import datetime
+from stable_baselines3 import SAC
+import torch as th
+from gym_unity.envs import UnityToGymWrapper
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
+from stable_baselines3.common.callbacks import CheckpointCallback, BaseCallback
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from typing import Callable
+from gym import spaces
+import gym
+
+class TupleToBoxWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(TupleToBoxWrapper, self).__init__(env)
+        
+        self.observation_space = spaces.Box(
+            low=np.concatenate([space.low for space in env.observation_space.spaces]),
+            high=np.concatenate([space.high for space in env.observation_space.spaces]),
+            dtype=np.float32
+        )
+
+    def observation(self, observation):
+        return np.concatenate(observation)
+
+def create_env(env_path, worker_id=0, time_scale=2.0, no_graphics=False):
+    channel = EngineConfigurationChannel()
+    unity_env = UnityEnvironment(env_path, side_channels=[channel], worker_id=22, no_graphics=no_graphics, base_port=5)
+    channel.set_configuration_parameters(time_scale=time_scale)
+    
+    gym_env = UnityToGymWrapper(unity_env, allow_multiple_obs=True)
+    gym_env = TupleToBoxWrapper(gym_env)
+    
+    return gym_env
+
+def get_save_path(base_path, model_name, trained_path=False):
+    date_str = datetime.now().strftime("%d%m%Y")
+    version = 0
+    if trained_path:
+        save_path = f"{base_path}/{model_name}_{date_str}_v{version}.zip"
+        while os.path.exists(save_path):
+            version += 1
+            save_path = f"{base_path}/{model_name}_{date_str}_v{version}.zip"
+    else:
+        save_path = f"{base_path}/{model_name}_{date_str}_v{version}"
+        while os.path.exists(save_path):
+            version += 1
+            save_path = f"{base_path}/{model_name}_{date_str}_v{version}"
+    return save_path[:-4] if trained_path else save_path
+
+class RewardLoggingCallback(BaseCallback):
+    def __init__(self, log_interval, base_path, model_name, verbose=0):
+        super(RewardLoggingCallback, self).__init__(verbose)
+        self.log_interval = log_interval
+        self.base_path = base_path
+        self.model_name = model_name
+        self.step_count = 0
+        self.total_reward = 0
+
+        # Generate the log file path with versioning
+        self.log_file = self.get_log_file_path()
+
+    def get_log_file_path(self):
+        save_path = get_save_path(self.base_path, self.model_name)
+        os.makedirs(save_path, exist_ok=True)
+        log_file = f"{save_path}/{self.model_name}_reward_log.txt"
+        version = 0
+        while os.path.exists(log_file):
+            version += 1
+            log_file = f"{save_path}/{self.model_name}_reward_log_v{version}.txt"
+        return log_file
+
+    def _on_step(self) -> bool:
+        self.step_count += 1
+        self.total_reward += np.sum(self.locals['rewards'])
+
+        if self.step_count % self.log_interval == 0:
+            with open(self.log_file, 'a') as f:
+                f.write(f'Step: {self.step_count}, Reward: {self.total_reward}\n')
+            print(f'Logged reward at step {self.step_count}: {self.total_reward}')
+            self.total_reward = 0
+        return True
+
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    def func(progress_remaining: float) -> float:
+        return progress_remaining * initial_value
+    return func
+
+if __name__ == '__main__':
+    n_envs = 1
+
+    env = r"continuous_path_build\3DPos.exe"
+    def make_env_scene1(worker_id):
+        return lambda: create_env(env, worker_id, time_scale=4.0, no_graphics=True)
+
+    env_fns_scene1 = [make_env_scene1(i) for i in range(n_envs)]
+    env_scene1 = SubprocVecEnv(env_fns_scene1)
+
+    base_path = 'continuous_path_models/logs_models'
+    trained_models_path = 'continuous_path_models/trained_models'
+    os.makedirs(trained_models_path, exist_ok=True)
+
+    model_name = 'sac'
+    save_path_scene1 = get_save_path(base_path, model_name)
+    checkpoint_callback_scene1 = CheckpointCallback(save_freq=250000, save_path=save_path_scene1, name_prefix=model_name)
+
+    policy_kwargs = dict(activation_fn=th.nn.ReLU,
+                         net_arch=[128, 256, 128])
+
+    tensorboard_log_path_scene1 = get_save_path("./logs_graphs", model_name)
+    reward_logging_callback_scene1 = RewardLoggingCallback(log_interval=1000, base_path=base_path, model_name=model_name)
+
+    device = th.device("cuda" if th.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    model = SAC("MlpPolicy", env_scene1, verbose=2, tensorboard_log=tensorboard_log_path_scene1,
+                policy_kwargs=policy_kwargs,
+                device=device)
+
+    model.learn(total_timesteps=2000000, reset_num_timesteps=True, tb_log_name="train_scene3",
+                callback=[checkpoint_callback_scene1, reward_logging_callback_scene1])
+
+    final_model_path = get_save_path(trained_models_path, model_name, trained_path=True)
+    model.save(final_model_path)
+    env_scene1.close()
---
+++ scripts/discrete_path_scripts/plotting_script.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def read_data(filename):
+    steps = []
+    rewards = []
+    with open(filename, 'r') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            step = int(parts[0].split(':')[1].strip())
+            reward = float(parts[1].split(':')[1].strip())
+            steps.append(step)
+            rewards.append(reward)
+    return steps, rewards
+
+def moving_average(data, window_size):
+    return pd.Series(data).rolling(window=window_size).mean()
+
+def plot_moving_average(steps, rewards, window_size, label):
+    moving_avg_rewards = moving_average(rewards, window_size)
+    plt.plot(steps, moving_avg_rewards, label=label)
+    plt.xlabel('Step')
+    plt.ylabel('Reward')
+    plt.title('Reward vs. Step with Moving Average')
+
+def main():
+    filenames = [
+        r'continuous_path_models\logs_models\ppo_31082024_v0\ppo_reward_log.txt',
+        r'continuous_path_models\logs_models\sac_31082024_v0\sac_reward_log.txt',
+        r'continuous_path_models\logs_models\TD3_31082024_v0\TD3_reward_log.txt'
+    ]
+
+    window_size = 100
+
+    for filename in filenames:
+        steps, rewards = read_data(filename)
+        label = filename.split("\\")[-2] 
+        plot_moving_average(steps, rewards, window_size, label)
+
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
